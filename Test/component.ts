4de1d9bcbd588e55d3ef8c4e89d70fcf0bb6cf07
PavloShymanskyj <32950068+PavloShymanskyj@users.noreply.github.com>
2019-06-25 22:47:12 +0300

    Test

---
+++ Test/component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  calendarPlugins = [dayGridPlugin]; // important!
+
+}
